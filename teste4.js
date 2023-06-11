de11a9962ab64d1f5dc0324bdda27f4959fcd684
Ed-Silverio-Mr-Booleano <edmilsondiabinho0@gmail.com>
2023-06-11 14:57:57 +0100

    refactor: ensure the test4 update user in correct way

--- teste4.js
+++ teste4.js
@@ -1,13 +1,25 @@
 var data =  require("./fakeData");
-
+const fs = require('fs');
 module.exports =  function(req, res) {
   
-    var id =  req.query.id;
+    var id =  req.params.id;
+    let job = req.body.job;
+    let name = req.body.name;
 
-    const reg = data.find(d => id == id);
-    reg.name = req.body.name;
-    reg.job = req.body.job;
 
-    res.send(reg);
+    data.map(dta =>{
+       if (dta.id == id){
+          dta.name = name;
+          dta.job = job;
+          return;
+       }
+    });
+
+    fs.writeFile("users.json", JSON.stringify(data), err => {
+        if (err) throw err;
+        res.send({msg: "This users was updated", name, job});
+    });
+
+
 
 };
